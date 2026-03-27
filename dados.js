a8af19952360d85b8c6ac88fcadd6ad2a32c5660
allansmaia <84999755+allansmaia@users.noreply.github.com>
2026-03-27 08:10:36 -0300

    Corrigido nome dos campos ID

--- dados.js
+++ dados.js
@@ -7,8 +7,8 @@ const bancoDeDados = {
         { codigo: "DESCONTO20", percentual: 20 },
         { codigo: "DESCONTO50", percentual: 50 }
     ],
-    proximosIdProduto: 1,
-    proximosIdPedido: 1
+    proximoIdProduto: 1,
+    proximoIdPedido: 1
 };
 
 module.exports = bancoDeDados;
