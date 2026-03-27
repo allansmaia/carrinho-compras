857d979a6b0fd883e8f1000a1b7d6a3ac52d8ef9
allansmaia <84999755+allansmaia@users.noreply.github.com>
2026-03-27 07:58:33 -0300

    Adicionado Estrutura de Dados

---
+++ dados.js
@@ -0,0 +1,14 @@
+const bancoDeDados = {
+    produtos: [],
+    carrinho: [],
+    pedidos: [],
+    cupons: [
+        { codigo: "DESCONTO10", percentual: 10 },
+        { codigo: "DESCONTO20", percentual: 20 },
+        { codigo: "DESCONTO50", percentual: 50 }
+    ],
+    proximosIdProduto: 1,
+    proximosIdPedido: 1
+};
+
+module.exports = bancoDeDados;
